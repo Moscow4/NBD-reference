4743454831bdba7128b59b8cb1364d3692c9c05b
OnyxFlames <jacobhicks1996@hotmail.com>
2017-12-14 18:01:43 -0500

    Added solution for NBDDatabase.dll

---
+++ NBDDatabase/NBDDatabase/Data.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBDDatabase
+{
+    internal class Data
+    {
+        internal static SqlConnection con = new SqlConnection();
+        static Data()
+        {
+            con.ConnectionString = global::NBDDatabase.Properties.Settings.Default.NBD_SBConnectionString;
+        }
+    }
+}
